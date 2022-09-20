e8b7dccdf04ee3ebca54b39904057282d7b51028
Gourab Mitra <gourabmitra93@gmail.com>
2022-09-20 15:56:14 +0530

    App renamed with new package name and display name for android platform only.

--- mobile/android/app/src/main/java/com/testproject/mobile/newarchitecture/MainApplicationReactNativeHost.java
+++ mobile/android/app/src/main/java/com/testproject/mobile/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.mobile.newarchitecture;
+package com.testproject.mobile.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.mobile.BuildConfig;
-import com.mobile.newarchitecture.components.MainComponentsRegistry;
-import com.mobile.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.testproject.mobile.BuildConfig;
+import com.testproject.mobile.newarchitecture.components.MainComponentsRegistry;
+import com.testproject.mobile.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
